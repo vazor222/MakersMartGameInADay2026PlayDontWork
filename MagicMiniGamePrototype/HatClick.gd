270944c5fb16c88f3003f9505f1586e78e3d8567
vazor <vazor222@gmail.com>
2026-05-08 17:51:21 -0600

    skill based minigame

--- MagicMiniGamePrototype/HatClick.gd
+++ MagicMiniGamePrototype/HatClick.gd
@@ -6,11 +6,10 @@ extends Area2D
 @export var animation_player:AnimationPlayer
 
 var shuffle_count_current:int
-var score:int
+var move_count:int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	score = 0
 	input_event.connect(_on_input_event)
 	if index != 2:
 		$BunnySprite2D.hide()
@@ -24,18 +23,14 @@ func _process(delta):
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	if Input.is_action_just_pressed('mouse_click'):
 		print('clicked! idx:', index)
-		var correct = get_tree().get_first_node_in_group('start_shuffle').get_correct_hat_index()
-		if index == correct:
-			score += 1
-			get_tree().get_first_node_in_group('magic_office').add_score(1)
+		get_tree().get_first_node_in_group('magic_office').clicked_hat_index(index)
 
 func _on_drop_finished():
 	get_tree().get_first_node_in_group('start_shuffle').continue_shuffle()
 	$BunnySprite2D.hide()
 
-func get_score() -> int:
-	return score
-
+func get_move_count() -> int:
+	return move_count
 
 func lift():
 	animation_player.play('hat_lift_up')
@@ -45,6 +40,7 @@ func drop():
 
 func start_shuffle():
 	shuffle_count_current = shuffle_count
+	move_count = 0
 	print('start shuffle count:',shuffle_count_current)
 
 func shuffle():
@@ -55,6 +51,7 @@ func shuffle():
 	print('shuffle2 index:',index,' shuffle count:',shuffle_count_current)
 	if shuffle_count_current == 0:
 		print('shuffle end')
+		animation_player.play('hat_idle')
 		if index == 0:
 			end_shuffle()
 		return
@@ -65,18 +62,32 @@ func shuffle():
 	if index == 0:
 		if target_pos == 1:
 			animation_player.play('hat_shuffle_left_to_middle')
+			move_count += 1
+			return
 		elif target_pos == 2:
 			animation_player.play('hat_shuffle_left_to_right')
+			move_count += 1
+			return
 	elif index == 1:
 		if target_pos == 0:
 			animation_player.play('hat_shuffle_middle_to_left')
+			move_count += 1
+			return
 		elif target_pos == 2:
 			animation_player.play('hat_shuffle_middle_to_right')
+			move_count += 1
+			return
 	elif index == 2:
 		if target_pos == 0:
 			animation_player.play('hat_shuffle_right_to_left')
+			move_count += 1
+			return
 		elif target_pos == 1:
 			animation_player.play('hat_shuffle_right_to_middle')
+			move_count += 1
+			return
+	
+	animation_player.play('hat_idle')
 
 func end_shuffle():
 	get_tree().get_first_node_in_group('start_shuffle').end_shuffle()
--- MagicMiniGamePrototype/magicOffice.gd
+++ MagicMiniGamePrototype/magicOffice.gd
@@ -31,3 +31,24 @@ func show_work():
 func add_score(amount):
 	score += amount
 	score_label.text = str(score)
+
+func clicked_hat_index(index):
+	var highestMoveCount:int = 0
+	var leftMoveCount = get_tree().get_first_node_in_group('hat_left').get_move_count()
+	if leftMoveCount > highestMoveCount:
+		highestMoveCount = leftMoveCount
+	var middleMoveCount = get_tree().get_first_node_in_group('hat_middle').get_move_count()
+	if middleMoveCount > highestMoveCount:
+		highestMoveCount = middleMoveCount
+	var rightMoveCount = get_tree().get_first_node_in_group('hat_right').get_move_count()
+	if rightMoveCount > highestMoveCount:
+		highestMoveCount = rightMoveCount
+	
+	if index == 0 && leftMoveCount == highestMoveCount:
+		add_score(1)
+	elif index == 1 && middleMoveCount == highestMoveCount:
+		add_score(1)
+	elif index == 2 && rightMoveCount == highestMoveCount:
+		add_score(1)
+	
+	pass
