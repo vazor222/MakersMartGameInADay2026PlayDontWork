17de8b9882e4240d84ec0c82dc0659eb9c84a592
dojeda1 <dominicojeda.coding@gmail.com>
2026-05-08 17:29:59 -0600

    added boss with animation.

---
+++ MagicMiniGamePrototype/WorkIconArea.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	input_event.connect(_on_input_event)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if Input.is_action_just_pressed('mouse_click'):
+		print('clicked! Work')
+		get_tree().get_first_node_in_group('magic_office').show_game()
---
+++ MagicMiniGamePrototype/magicOffice.gd
@@ -0,0 +1,26 @@
+extends Node2D
+@onready var game_window = $Node2D/GameWindow
+@onready var hat_game = $HatGame
+@onready var worksheet_window = $Node2D/WorksheetWindow
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func show_game():
+	hat_game.show()
+	game_window.show()
+	
+	worksheet_window.hide()
+
+func show_work():
+	worksheet_window.show()
+	
+	game_window.hide()
+	hat_game.hide()
