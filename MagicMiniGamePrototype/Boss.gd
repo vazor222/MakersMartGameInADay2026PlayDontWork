ba0a9b5bc1f638a9ec61c96f3f62f7dae190f65a
dojeda1 <dominicojeda.coding@gmail.com>
2026-05-09 09:57:00 -0600

    added boss animation randomizer.

---
+++ MagicMiniGamePrototype/Boss.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_animation_player_animation_finished(anim_name):
+	var animations = animation_player.get_animation_list()
+	animation_player.play(animations[randi() % animations.size()])
+	pass # Replace with function body.
